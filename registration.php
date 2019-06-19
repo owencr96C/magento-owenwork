b503d69935dd26ea31c927f88606d0a67b5434fc
vagrant <vagrant@homestead>
2019-06-19 13:14:39 +0000

    initialise module

---
+++ registration.php
@@ -0,0 +1,10 @@
+<?php
+/**
+ * @package  Costa_Owenwork
+ * @author   Whitbread
+ */
+\Magento\Framework\Component\ComponentRegistrar::register(
+    \Magento\Framework\Component\ComponentRegistrar::MODULE,
+    'Costa_Owenwork',
+    __DIR__
+);
