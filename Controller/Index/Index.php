4361d2554f431e81e6bb74de0ed336d369297d9c
vagrant <vagrant@homestead>
2019-06-24 07:23:24 +0000

    fix controller

--- Controller/Index/Index.php
+++ Controller/Index/Index.php
@@ -8,6 +8,10 @@ class Index extends \Magento\Framework\App\Action\Action
 {
     public function execute()
     {
-        return $this->resultFactory->create(ResultFactory::TYPE_PAGE); //Just return a page, which can be configured in the layout
+        /** @var @var \Magento\Framework\Controller\Result\Raw $result */
+        $result = $this->resultFactory->create(ResultFactory::TYPE_RAW); // A RAW result will ignore all layouts and other settings in magento, but we want out controller to render the layout
+        $result->setContents('Hello Team!');
+        return $result;
+        //return $this->resultFactory->create(ResultFactory::TYPE_PAGE); //Just return a page, which can be configured in the layout
     }
 }
